118eed7cd5a78586efb0431502c1ae283a9bc15a
farahaiman <farahaiman548@gmail.com>
2020-07-25 20:41:13 +0500

    38 to 42 chapter complete

--- app.js
+++ app.js
@@ -1743,6 +1743,105 @@
 //}
 //Result(100,100,100);
             //Q5
-   
-         
+   // var NewArrayOne = ["a","b","c","d","e"];
+// alert("Array: "+NewArrayOne)
+// var i = 0;
+// do {
+//   if(NewArrayOne === "c"){
+//       alert(NewArrayOne.length);
+//   }
+// }
+// while (i < NewArrayOne.length);
+ // Task # 6
+
+// function removeVowels(){
+//     var strOne = ["apple","banana","grapes","incognito","universe"];                          
+
+//    string = strOne.map(x=>x.replace( /[aeiou]/g, '' ));              
+
+//   alert(string);
+// }
+// removeVowels();
+           // Task # 7
+
+// function findOccurrences() {
+//     var str = "Pleases read this application and give me gratuity";
+//     var count = 0;
+//     let haveSeenVowel = false;
+    
+//     for (const letter of str.toLowerCase()) {
+//       switch (letter) {
+//         case 'a':
+//         case 'e':
+//         case 'i':
+//         case 'o':
+//         case 'u':
+//           {
+//             if (haveSeenVowel) {
+//               count++;
+//               haveSeenVowel = false;
+//             } else {
+//               haveSeenVowel = true;
+//             }
+//             break;
+//           }
+//         default:
+//           haveSeenVowel = false
+//       }
+//     }
+//     return count
+//   }
+           //   alert("Occurance of vowels: "+findOccurrences());
+
+// TAsk # 8
+
+// var distance = +prompt("Enter distance between two cities in KM");
+// function ConvertToMeter(){
+//     var meter = distance*1000;
+//     return meter
+// }
+// function ConvertToFeet(){
+//     var feet = distance*3281;
+//     return feet
+// }function ConvertToInches(){
+//     var inches = distance*39370
+//     return inches
+// }function ConvertToCentiMeter(){
+//     var cm = distance*100000
+//     return cm
+// }
+// alert(distance+"KM into meter is: "+ConvertToMeter());
+// alert(distance+"KM into feet is: "+ConvertToFeet());
+// alert(distance+"KM into inches is: "+ConvertToInches());
+// alert(distance+"KM into centimeter is: "+ConvertToCentiMeter());
+           // Task # 9
+
+
+// function CalculateOvertime(){
+//     var WorkTime = +prompt("enter your complete working time in hours")
+//     var pay = 1000;
+//     alert("Your initial pay was: "+pay)
+//     if (WorkTime > 40){
+//         for(i = 41; i <= WorkTime; i++){
+//             pay += 12;
+//         }
+//         alert("After over time, your pay is: "+pay);
+//     }
+//     else{
+//         alert("no overtime");
+//     }
+// }
+// CalculateOvertime()
+     // Task # 10
+
+// function notes(){
+//     var withdraw = +prompt("enter amount you want to withdraw")
+//     var hundred = withdraw/100;
+//     var fifty = ((withdraw % 100) / 50);
+//     var ten = (((withdraw % 100) % 50) / 10);
+//     alert("Your amount is: "+withdraw+"\nYou'll have "+Math.floor(hundred)+" note(s) of hundred\n"+Math.floor(fifty)+" note(s) of fifty\n"+"And "+Math.floor(ten)+" note(s) of ten");
+// }
+// notes()      
+ ///........................................................................................,,     
+           // Chap # 43-48
     
